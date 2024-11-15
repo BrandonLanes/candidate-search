7e5ea1a29703e86ecc0543463446314d730fc4a8
Brandon <brandonlanes@gmail.com>
2024-11-15 00:09:06 -0500

    added code to nav.tsx to display the nav bar and links between pages, added code to candidatesearch.tsx to search for candidates and choose to save them or move along to the next one

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="./pages/CandidateSearch">Candidate Search</Link>
+        </li>
+        <li>
+          <Link to="./pages/ErrorPage">Error Page</Link>
+        </li>
+        <li>
+          <Link to="./pages/SavedCandidates">Saved Candidates</Link>
+        </li>
+      </ul>
+    </nav>
   )
 };
 
